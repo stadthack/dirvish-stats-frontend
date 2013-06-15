d3ab7d06b083f76b3f66330c3090936132ce3016
Tristan Koch <tristan@tknetwork.de>
2013-06-15 18:23:03 +0200

    Refactor

--- ui/app/scripts/controllers.js
+++ ui/app/scripts/controllers.js
@@ -12,19 +12,28 @@ angular.module('dirvishStatsApp')
   })
   .controller('ImagesCtrl', function($scope, $routeParams, $http) {
     var hostId = $routeParams.hostId;
-    if (hostId) {
-      for (var i = 0; i < $scope.hosts.length; i++) {
+
+    var findHost = function(hosts, hostId) {
+      if (!hosts.length || hostId == null) {
+        return;
+      };
+
+      for (var i = 0; i < hosts.length; i++) {
         var host = $scope.hosts[i];
         if (host.name == hostId) {
-          $scope.images = host.images;
-          break;
+          return host;
         }
       };
-    }
+    };
 
-    $scope.trendImages = []
-    $http({method: "GET", url: "http://fs1.phimobile.com:8000/trends?hostname="+hostId}).success(function(data) {
+    $scope.trendImages = [];
+    $http.get("http://fs1.phimobile.com:8000/trends?hostname=" + hostId).success(function(data) {
       $scope.trendImages = data.images;
+    });
+
+    $scope.$watch('hosts', function() {
+      $scope.host = findHost($scope.hosts, hostId);
+      $scope.images = $scope.host ? $scope.host.images : [];
     });
 
     //
