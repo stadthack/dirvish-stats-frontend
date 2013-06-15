2026391c8492445de58942896ee4df2f80fc4e08
Tristan Koch <tristan@tknetwork.de>
2013-06-16 01:06:15 +0200

    Fix indendation and remove $routeProvider

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -1,32 +1,32 @@
 'use strict';
 
 angular.module('dirvishStatsApp', ['ui.state'])
-  .config(function($stateProvider, $routeProvider) {
+  .config(function($stateProvider) {
     $stateProvider
-    .state('index', {
-      url: '',
-      views: {
-        'main': {
-          templateUrl: 'views/hosts.html'
+      .state('index', {
+        url: '',
+        views: {
+          'main': {
+            templateUrl: 'views/images.html'
+          }
         }
-      }
-    })
-    .state('images', {
-      url: '/hosts/:hostId/images',
-      views: {
-        'main': {
-          templateUrl: 'views/images.html'
+      })
+      .state('images', {
+        url: '/hosts/:hostId/images',
+        views: {
+          'main': {
+            templateUrl: 'views/images.html'
+          }
         }
-      }
-    })
-    .state('images.detail', {
-      url: '/:imageId',
-      views: {
-        'imagesDetail': {
-          templateUrl: 'views/files.html'
+      })
+      .state('images.detail', {
+        url: '/:imageId',
+        views: {
+          'imagesDetail': {
+            templateUrl: 'views/files.html'
+          }
         }
-      }
-    });
+      });
   })
   .config(function($httpProvider) {
     $httpProvider.defaults.headers.common['X-Requested-With'] = '';
