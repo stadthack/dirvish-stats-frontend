e2a046ae07a2b71cb034f07e0b0239d6a9868689
Tristan Koch <tristan@tknetwork.de>
2013-06-15 15:13:26 +0200

    Load trend.json in bar graph

--- ui/app/scripts/controllers.js
+++ ui/app/scripts/controllers.js
@@ -58,40 +58,36 @@ angular.module('dirvishStatsApp')
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var data = [
-      {letter: 'A', frequency: 0.1},
-      {letter: 'B', frequency: 0.2},
-      {letter: 'C', frequency: 0.7},
-    ];
+    $scope.$watch("trendImages", function(data) {
+      data.forEach(function(d) {
+        d.sum = +d.sum;
+      });
 
-    data.forEach(function(d) {
-      d.frequency = +d.frequency;
+      x.domain(data.map(function(d, i) { return i; }));
+      y.domain([0, d3.max(data, function(d) { return d.sum; })]);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("sum");
+
+      svg.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.time); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.sum); })
+          .attr("height", function(d) { return height - y(d.sum); });
     });
-
-    x.domain(data.map(function(d) { return d.letter; }));
-    y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
-
-    svg.selectAll(".bar")
-        .data(data)
-      .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.letter); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d.frequency); })
-        .attr("height", function(d) { return height - y(d.frequency); });
   });
