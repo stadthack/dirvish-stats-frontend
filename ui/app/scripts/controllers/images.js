27856b6aed120794de98f18e6351c901875e8346
Tristan Koch <tristan@tknetwork.de>
2013-06-16 01:06:46 +0200

    Brutal hack to show first host on root url
    NOTE: First host is hard-coded. Please fix me.

--- ui/app/scripts/controllers/images.js
+++ ui/app/scripts/controllers/images.js
@@ -4,6 +4,10 @@ angular.module('dirvishStatsApp')
   .controller('ImagesCtrl', function($scope, $state, $http) {
     var hostId = $state.params.hostId;
 
+    // TODO: Handle in router
+    hostId = hostId || 'beamdb1';
+    $state.params.hostId = hostId;
+
     var findHost = function(hosts, hostId) {
       if (!hosts.length || hostId == null) {
         return;
