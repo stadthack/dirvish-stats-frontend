5b163c69a4c84081448eb2a4ce234da2646709d3
Tristan Koch <tristan@tknetwork.de>
2013-06-15 12:41:44 +0200

    Load hosts using $http service

--- ui/app/scripts/controllers.js
+++ ui/app/scripts/controllers.js
@@ -1,20 +1,16 @@
 'use strict';
 
-var hosts = [
-  {name: "Host-A", images: [{name: "2013-01-01"}, {name: "2013-02-01"}, {name: "2013-03-01"}]},
-  {name: "Host-B", images: [{name: "2012-01-01"}, {name: "2012-02-01"}, {name: "2012-03-01"}]},
-  {name: "Host-C", images: [{name: "2011-01-01"}, {name: "2011-02-01"}, {name: "2011-03-01"}]}
-]
-
 angular.module('dirvishStatsApp')
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope, $http) {
+    $scope.hosts = [];
+    $http({method: "GET", url: "hosts.json"}).success(function(data) {
+      $scope.hosts = data.hosts;
+    });
   })
   .controller('HostsNavCtrl', function($scope) {
-    $scope.hosts = hosts;
   })
   .controller('ImagesCtrl', function($scope, $routeParams) {
     var hostId = $routeParams.hostId;
-    $scope.hosts = hosts;
     if (hostId) {
       for (var i = 0; i < $scope.hosts.length; i++) {
         var host = $scope.hosts[i];
