fd86c2842fc099d1a87322a33aeb92dbf1b84650
Andreas Haller <andreashaller@gmail.com>
2013-06-16 02:08:06 +0200

    Refactor: No need to define ui-view name

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -3,30 +3,22 @@
 angular.module('dirvishStatsApp', ['ui.state'])
   .config(function($stateProvider) {
     $stateProvider
-      .state('index', {
-        url: '',
-        views: {
-          'main': {
-            templateUrl: 'views/images.html'
-          }
+    .state('index', {
+      url: '',
+      templateUrl: 'views/images.html'
+    })
+    .state('images', {
+      url: '/hosts/:hostId/images',
+      templateUrl: 'views/images.html'
+    })
+    .state('images.detail', {
+      url: '/:imageId',
+      views: {
+        'imagesDetail': {
+          templateUrl: 'views/files.html'
         }
-      })
-      .state('images', {
-        url: '/hosts/:hostId/images',
-        views: {
-          'main': {
-            templateUrl: 'views/images.html'
-          }
-        }
-      })
-      .state('images.detail', {
-        url: '/:imageId',
-        views: {
-          'imagesDetail': {
-            templateUrl: 'views/files.html'
-          }
-        }
-      });
+      }
+    })
   })
   .config(function($httpProvider) {
     $httpProvider.defaults.headers.common['X-Requested-With'] = '';
